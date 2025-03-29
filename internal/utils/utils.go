f9be51afd347050b78906d8abbb4858bc3b1ac06
lionel.wilson <lwilson@cynergybank.co.uk>
2025-03-29 21:24:23 +0000

    remove unused functions and folders

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,66 +1,8 @@
 package utils
 
 import (
-	"net/http"
-	"strconv"
 	"unicode"
-
-	"github.com/gin-gonic/gin"
 )
-
-// ErrorResponse represents the structure of an error response.
-// It contains a status code, a message, and an optional list of errors.
-type ErrorResponse struct {
-	StatusCode int      `json:"statusCode" example:"422"`
-	Message    string   `json:"message" example:"Validation failed"`
-	Errors     []string `json:"errors,omitempty"`
-}
-
-// NewErrorResponse creates a new error response with the provided status code, message, and errors.
-// It sends a JSON response with these details to the client.
-func NewErrorResponse(c *gin.Context, statusCode int, message string, errors []string) {
-	c.JSON(statusCode, ErrorResponse{
-		StatusCode: statusCode,
-		Message:    message,
-		Errors:     errors,
-	})
-}
-
-func ServerErrorResponse(c *gin.Context, err error, msg string) {
-	var message string
-	if msg != "" {
-		message = msg
-	} else {
-		message = "Something went wrong. Please try again later."
-	}
-
-	var errorDetails []string
-	if err != nil {
-		errorDetails = append(errorDetails, err.Error())
-	} else {
-		errorDetails = append(errorDetails, "Unknown error")
-	}
-
-	c.JSON(http.StatusInternalServerError, ErrorResponse{
-		StatusCode: http.StatusInternalServerError,
-		Message:    message,
-		Errors:     errorDetails,
-	})
-}
-
-func ExtractIntegerCookie(c *gin.Context, cookieName string) (int, error) {
-	cookieValueAsString, err := c.Request.Cookie(cookieName)
-	if err != nil {
-		return 0, err
-	}
-
-	cookieValueAsInt, err := strconv.Atoi(cookieValueAsString.Value)
-	if err != nil {
-		return 0, err
-	}
-
-	return cookieValueAsInt, nil
-}
 
 // containsNumber checks if a given string contains a number.
 func ContainsNumber(s string) bool {
